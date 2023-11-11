f133c4ede85f39d9002a260149ab0789ace63d72
gkfat <gkgkdesign@gmail.com>
2023-11-11 18:27:59 +0800

    style: add eslint config, tsconfig

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+/* eslint-env node */
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint'
+    ],
+    root: true,
+    rules: {
+        indent: ['error', 4],
+    },
+    ignorePatterns: [
+        "dist/**/*.js",
+    ],
+};
